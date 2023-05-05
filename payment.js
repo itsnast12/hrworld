b17fe3985c4c3aceb0475e670ca63777ca6a7359
Анастасия Терентьева <itsnast12@gmail.com>
2023-05-05 22:42:07 +0300

    payment

---
+++ payment.js
@@ -0,0 +1,7 @@
+document.querySelector(".signin").addEventListener("click", ()=>{
+	document.querySelector(".dropdown-list2").classList.toggle("dropdown-list--visible");
+})
+
+document.querySelector(".down").addEventListener("click", ()=>{
+	document.querySelector(".dropdown-list1").classList.toggle("dropdown-list--visible");
+})
